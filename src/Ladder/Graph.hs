1236f44f9842e0e5aeb58465706f4862c49d94f1
Sergei Mironenko <sergeimail@gmail.com>
2014-10-24 17:30:01 +0300

    use lazy graph instead of hashmap

--- src/Ladder/Graph.hs
+++ src/Ladder/Graph.hs
@@ -2,24 +2,24 @@ module Ladder.Graph where
 
 import Data.List(intercalate)
 import qualified Data.Set as S
-import qualified Data.HashMap.Strict as HM
 import qualified Data.Graph.AStar as A
 
 newtype Ladder = Ladder [String]
 instance Show Ladder where
   show (Ladder xs) = intercalate " -> " xs
 
-type WordGraph = HM.HashMap String [String]
+type WordGraph = (S.Set String, [String])
 
 distance :: String -> String -> Int
 distance a b = foldr (\(la,lb) total -> if (la == lb) then total else total+1) 0 (zip a b)
 
 buildGraph :: [String] -> WordGraph
-buildGraph ws = foldr (\k m -> HM.insert k (filter (\w -> distance w k == 1) ws) m) HM.empty ws
+buildGraph ws = (S.fromList ws, ws)
 
 shortestPath :: WordGraph -> String -> String -> Maybe [String]
-shortestPath dict start finish = A.aStar graph (\_ _ -> 1) (\w -> distance w finish) (\w -> w == finish) start
-                               where graph = \w -> case HM.lookup w dict of
-                                                    Nothing -> S.empty
-                                                    Just s -> S.fromList s
+shortestPath graph start finish =
+  A.aStar fngraph (\_ _ -> 1) (\w -> distance w finish) (\w -> w == finish) start
+    where fngraph v = if (S.member v (fst graph))
+                      then S.fromList $ filter (\wrd -> distance wrd v == 1) (snd graph)
+                      else S.empty
 
